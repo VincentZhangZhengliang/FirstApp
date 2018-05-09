d9baf8629deaf2e834e7374cb78d5de97d271eff
VincentZhang <529889220@>
2018-05-09 18:07:34 +0800

    ddddd

---
+++ app/src/main/java/com/joker/firstapp/ui/transition/TransitionAdapter.kt
@@ -0,0 +1,9 @@
+package com.joker.firstapp.ui.transition
+
+/**
+ * Created by Vincent;
+ * Created on 2018/5/9;
+ * DSC:
+ */
+class TransitionAdapter {
+}
---
+++ app/src/main/java/com/joker/firstapp/ui/transition/TransitionContentActivity.kt
@@ -0,0 +1,12 @@
+package com.joker.firstapp.ui.transition
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TransitionContentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transition_content)
+    }
+}
