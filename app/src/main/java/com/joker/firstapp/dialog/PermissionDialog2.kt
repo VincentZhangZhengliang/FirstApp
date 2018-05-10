53757862c23b821dbcd60d2962fcd7a20f7a8546
Joker_z <529889220@qq.com>
2018-05-10 23:26:40 +0800

    dialog

--- app/src/main/java/com/joker/firstapp/dialog/PermissionDialog2.kt
+++ app/src/main/java/com/joker/firstapp/dialog/PermissionDialog2.kt
@@ -1,20 +1,87 @@
 package com.joker.firstapp.dialog
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import com.joker.firstapp.R
+import kotlinx.android.synthetic.main.dialog_permission.*
 
 /**
  * Created by Vincent;
  * Created on 2018/5/10;
  * DSC:
  */
-class PermissionDialog2(context: Context) : AlertDialog(context) {
+class PermissionDialog2(context : Context) : AlertDialog(context, R.style.MyDialog) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var mContext : Context = context
+
+    private lateinit var onPositiveListener : OnPositiveListener
+    private lateinit var onNagetiveListener : OnNagetiveListener
+    private var positiveBtnStr : String = "确认"
+    private var nagetiveBtnStr : String = "取消"
+    private var titleStr : String = ""
+    private var contentStr : String = ""
+
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_permission)
+        setCanceledOnTouchOutside(false)
+        initLayoutParams(mContext)
+        initData()
+        initListener()
+    }
+
+    private fun initListener() {
+        dialog_permission_btn_cancel.setOnClickListener { onNagetiveListener.onClick() }
+        dialog_permission_btn_confirm.setOnClickListener { onPositiveListener.onClick() }
+    }
+
+    private fun initData() {
+
+        dialog_permission_tv_title.text = titleStr
+        dialog_permission_tv_content.text = contentStr
+        dialog_permission_btn_cancel.text = nagetiveBtnStr
+        dialog_permission_btn_confirm.text = positiveBtnStr
+
+    }
+
+    fun setTitle(str : String?) {
+        titleStr = str ?: ""
+    }
+
+    fun setContent(str : String?) {
+        contentStr = str ?: ""
+    }
+
+    private fun initLayoutParams(context : Context) {
+        val windowManager = (context as Activity).windowManager
+        val display = windowManager.defaultDisplay
+        val lp = window !!.attributes
+        // 设置dialog宽度为屏幕的4/5
+        lp.width = display.width * 4 / 5
+        // p.height = display.height / 3
+        window !!.attributes = lp
+    }
+
+    fun setOnPositiveListener(str : String?, listener : OnPositiveListener) {
+        positiveBtnStr = str ?: "确认"
+        onPositiveListener = listener
+    }
+
+    fun setOnNagetiveListener(str : String?, listener : OnNagetiveListener) {
+        nagetiveBtnStr = str ?: "取消"
+        onNagetiveListener = listener
+    }
+
+    interface OnPositiveListener {
+        fun onClick()
+    }
+
+    interface OnNagetiveListener {
+        fun onClick()
     }
 
 }
+
+
--- app/src/main/java/com/joker/firstapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/joker/firstapp/ui/splash/SplashActivity.kt
@@ -10,28 +10,52 @@ import com.yanzhenjie.permission.Rationale
 
 class SplashActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        AndPermission.with(this@SplashActivity).permission(android.Manifest.permission.READ_PHONE_STATE)
-                .rationale(mRationale)
-                .onGranted {}
-                .onDenied { }
-                .start()
+        AndPermission.with(this@SplashActivity).permission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE).rationale(mRationale).onGranted {}.onDenied {
+            if (AndPermission.hasAlwaysDeniedPermission(this@SplashActivity, it)) {
+                val settingService = AndPermission.permissionSetting(this@SplashActivity)
+                val dialog = PermissionDialog2(this@SplashActivity)
+                dialog.apply {
+                    setTitle("所有的安静都是人造的冷清")
+                    setContent("所有的安静都是人造的冷清")
+                    setOnNagetiveListener("拒绝", object : PermissionDialog2.OnNagetiveListener {
+                        override fun onClick() {
+                            // 如果用户不同意去设置：
+                            settingService.cancel()
+                            dismiss()
+                        }
+                    })
+                    setOnPositiveListener("去授权", object : PermissionDialog2.OnPositiveListener {
+                        override fun onClick() {
+                            // 如果用户同意去设置：
+                            settingService.execute()
+                            dismiss()
+                        }
+                    })
+                }
+                dialog.show()
+            }
+        }.start()
     }
 
-
-    private val mRationale = Rationale { context, permissions, executor ->
-        val ft = supportFragmentManager.beginTransaction()
-        val prev = supportFragmentManager.findFragmentByTag("dialog")
-        if (prev != null) {
-            ft.remove(prev)
-        }
-        ft.addToBackStack(null)
-        val dialog = PermissionDialog2(this@SplashActivity)
-        dialog.show()
-
+    private val mRationale = Rationale { _, _, executor ->
+        PermissionDialog2(this@SplashActivity).apply {
+            setTitle("所有的安静都是人造的冷清")
+            setContent("所有的安静都是人造的冷清")
+            setOnNagetiveListener("拒绝", object : PermissionDialog2.OnNagetiveListener {
+                override fun onClick() {
+                    executor.cancel()
+                    dismiss()
+                }
+            })
+            setOnPositiveListener("去授权", object : PermissionDialog2.OnPositiveListener {
+                override fun onClick() {
+                    executor.execute()
+                    dismiss()
+                }
+            })
+        }.show()
     }
-
-
 }
